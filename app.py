5acfbc1235f063152ea1f5f39b229bb6fda9c0c5
Ross <ross.e.macd@gmail.com>
2017-04-14 18:43:48 -0300

    Fixing

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-	def index(): return render_template('index.html')
+def index(): 
+    return render_template('index.html')
 
 @app.route("/hello")
 def hello():
