77eaf8c8645fd5934f73b882f5710dd8df5769dd
macdre <ross.e.macd@gmail.com>
2019-03-18 20:16:58 -0300

    yeah

--- src/js/components/WordItem.vue
+++ src/js/components/WordItem.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="word-item" v-bind:class="{'is-complete':word.operation}">
     <p>
-      <!--<input type="checkbox" v-on:change="toggleOperation">-->
       <a v-on:click="toggleOperation"> 
-        <!--<i :icon="[this.word.operation ? 'minus-square' : 'plus-square']" v-on:change="toggleOperation"/>-->
-        <font-awesome-icon :icon="this.word.operation ? 'minus-square' : 'plus-square'" size="2x" />
+        <font-awesome-icon :icon="this.word.operation ? 'minus-square' : 'plus-square'" size="2x" class="word-operation"/>
       </a>    
       {{word.content}}
       <button @click="$emit('del-word', word.id)" class="del">x</button>
@@ -31,8 +29,12 @@ export default {
     border-bottom: 1px #ccc dotted;
   }
 
+  .word-operation {
+    padding: 5px;
+  }
+
   .is-complete {
-    text-decoration: line-through;
+
   }
 
   .del {
