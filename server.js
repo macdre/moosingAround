1edc2175629b95809027c651957a45c6fd77c446
macdre <ross.e.macd@gmail.com>
2019-03-23 12:31:33 -0300

    api routing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var http = require('http');
 var proxy = require('http-proxy-middleware');
-var fs = require('fs');
+//var fs = require('fs');
 var serveStatic = require('serve-static');
 var port = process.env.PORT || 5000;
 
@@ -10,18 +10,26 @@ app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
 // Config
-const { routes } = require('./proxyconfig.json');
-for (route of routes) {
-    app.use(route.route,
-        proxy({
-            target: route.address,
-            pathRewrite: (path, req) => {
-                return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
-            }
-        })
-    );
-}
+// const { routes } = require('./proxyconfig.json');
+// for (route of routes) {
+//     app.use(route.route,
+//         proxy({
+//             target: route.address,
+//             pathRewrite: (path, req) => {
+//                 return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
+//             }
+//         })
+//     );
+// }
 
+app.all('/api/*', (req, res) => {
+    const __path = _.drop(req.url.split('/'), 2);
+    proxy.proxyRequest(req, res, {
+      target: url.resolve('http://localhost', __path.join('/')),
+      port: 9090,
+      ignorePath: true
+    });
+});
 
 http.createServer(app).listen(port);
 console.log('server started '+ port);
