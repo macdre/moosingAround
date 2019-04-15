79392752aa1d6c5d91c3ea479d711072ad33b369
macdre <ross.e.macd@gmail.com>
2019-04-15 19:28:49 -0300

    adding cors support

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 var express = require('express');
 var serveStatic = require('serve-static');
 var port = process.env.PORT || 5000;
+var cors = require('cors');
+
 app = express();
+app.use(cors());
 app.use(serveStatic(__dirname + "/dist"));
 app.listen(port);
 console.log('server started '+ port);
