c3cd5d6fc3789ec12e6b598772116505c10cfa44
macdre <ross.e.macd@gmail.com>
2019-04-15 19:37:20 -0300

    Adding specific cors header configuration

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ var port = process.env.PORT || 5000;
 var cors = require('cors');
 
 app = express();
-app.use(cors());
+//app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(serveStatic(__dirname + "/dist"));
 app.listen(port);
 console.log('server started '+ port);
